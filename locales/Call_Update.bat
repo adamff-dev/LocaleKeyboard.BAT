ce5ef38e71830f4f50af2d083c127aa2768b2441
BlueArduino20 <bluearduino20@gmail.com>
2017-05-06 11:45:24 +0200

    Update Call_Update.bat

--- locales/Call_Update.bat
+++ locales/Call_Update.bat
@@ -6,41 +6,44 @@ cls
 ECHO Updating... Please, wait.
 del *.lang
 cls
-cscript .Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/en_US.lang
+cscript Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/en_US.lang >nul
 cls
 ECHO Updating... Please, wait.
-cscript .Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/en_UK.lang
+cscript Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/en_UK.lang >nul
 cls
 ECHO Updating... Please, wait.
-cscript .Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/de_DE.lang
+cscript Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/de_DE.lang >nul
 cls
 ECHO Updating... Please, wait.
-cscript .Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/fr_FR.lang
+cscript Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/fr_FR.lang >nul
 cls
 ECHO Updating... Please, wait.
-cscript .Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/be_BE.lang
+cscript Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/be_BE.lang >nul
 cls
 ECHO Updating... Please, wait.
-cscript .Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/es_ES.lang
+cscript Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/es_ES.lang >nul
 cls
 ECHO Updating... Please, wait.
-cscript .Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/fi_FI.lang
+cscript Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/fi_FI.lang >nul
 cls
 ECHO Updating... Please, wait.
-cscript .Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/cz_CZ.lang
+cscript Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/cz_CZ.lang >nul
 cls
 ECHO Updating... Please, wait.
-cscript .Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/da_DK.lang
+cscript Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/da_DK.lang >nul
 cls
 ECHO Updating... Please, wait.
-cscript .Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/pt_PT.lang
+cscript Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/pt_PT.lang >nul
 cls
 ECHO Updating... Please, wait.
-cscript .Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/tr_TR.lang
+cscript Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/tr_TR.lang >nul
 cls
 ECHO Updating... Please, wait.
-cscript .Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/it_IT.lang
+cscript Updater.vbs https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/it_IT.lang >nul
 cls
-echo Done!
-pause
+echo -------------------------------------
+echo                 Done! 
+echo       Press any key to continue.
+echo -------------------------------------
+pause >nul
 exit
